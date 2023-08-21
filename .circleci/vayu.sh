a64b2bd79ecc01f2b365d51e0e7ac1d4a4a769bd
fredslbn <fh.silaban@gmail.com>
2023-08-21 23:34:44 +0700

    switch to cosmic clang

--- .circleci/vayu.sh
+++ .circleci/vayu.sh
@@ -48,7 +48,7 @@ FINAL_ZIP_ALIAS=Karenulgarde-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=clang17-7
+COMPILER=cosmic-clang
 
 ##----------------------------------------------------------##
 # Specify Linker
@@ -356,8 +356,8 @@ START=$(date +"%s")
 	       OBJCOPY=llvm-objcopy \
 	       OBJDUMP=llvm-objdump \
 	       STRIP=llvm-strip \
-	       #READELF=llvm-readelf \
-	       #OBJSIZE=llvm-size \
+	       READELF=llvm-readelf \
+	       OBJSIZE=llvm-size \
 	       V=$VERBOSE 2>&1 | tee error.log
 	       
 	elif [ -d ${KERNEL_DIR}/gcc64 ];
